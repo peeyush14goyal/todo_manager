77c685992475a12081151fd2d2ea094932be6a30
Peeyush <peeyushgoyal154@gmail.com>
2021-01-19 19:19:57 +0530

    Todo Application with UI

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,13 +4,15 @@ class TodosController < ApplicationController
 
   def index
     #render plain: Todo.all.map { |todo| todo.to_pleasant_string }.join("\n")
-    render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
+    #render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
+    render "index"
   end
 
   def show
     id = params[:id]
     todo = Todo.find(id)
-    render plain: todo.to_pleasant_string
+    render "todo"
+    #render plain: todo.to_pleasant_string
   end
 
   def create
