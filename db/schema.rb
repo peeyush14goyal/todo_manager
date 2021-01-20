46cefe79237db5e5d338dd41bee1da66935ef442
Peeyush <peeyushgoyal154@gmail.com>
2021-01-20 15:24:39 +0530

    Ran migration for deleting old users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_19_135629) do
+ActiveRecord::Schema.define(version: 2021_01_20_095142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,14 +19,6 @@ ActiveRecord::Schema.define(version: 2021_01_19_135629) do
     t.text "todo_text"
     t.date "due_date"
     t.boolean "completed"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
