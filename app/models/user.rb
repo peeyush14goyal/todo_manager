606c9e8a9f4c337ac9054738e577a8ecc07550ed
Peeyush <peeyushgoyal154@gmail.com>
2021-01-21 13:57:47 +0530

    Added User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :email, presence: true
+  validates :password, length: { minimum: 3 }
   has_secure_password
   has_many :todos
 end
